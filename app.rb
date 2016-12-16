dde098db08dcd58f3373d06d8b6a6a51b54609ef
Margie Nevarez <margie.ynm@gmail.com>
2016-12-15 21:40:25 -0800

    create routing and display for index

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ Bundler.require(:default)
 require("pry")
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @stores = Store.all()
+  @brands = Brand.all()
+  erb(:index)
+end
